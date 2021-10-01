6ac3f49778e2964711eb4e71f44e838f0fd73a41
Sema Novik <novikreef@gmail.com>
2021-10-01 12:20:23 +0200

    Чуть изменил локаторы

--- Part #5/#5.1.py
+++ Part #5/#5.1.py
@@ -4,27 +4,23 @@ import time
 
 # Запускаем браузер
 browser = wd.Chrome()
-browser.get('https://sbis.ru/')
+try:
+    browser.get('https://sbis.ru/')
+    browser.find_element_by_css_selector('[href="/support"]').click()
 
-# Переход к странице с ссылками, поочередное ожидание подгрузки элементов страницы
-browser.implicitly_wait(20)
-browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div[1]/div[2]/div[5]/div[5]').click()
+    # Скролим до кнопки, которая в центре страницы
+    hidden_button = browser.find_element_by_css_selector('[href="/download"]')
+    browser.execute_script("return arguments[0].scrollIntoView(true);", hidden_button)
+    hidden_button.click()
 
-browser.implicitly_wait(20)
-browser.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/div/div/div[4]/a').click()
+    # Список параметров для поиска эелементов
+    links_text = ['Полная версия (294.40 MB)', 'Обновление (229.75 MB)', 'Полная версия (66.53 MB)',
+                  'Скачать (6.4 МБ)', 'Скачать (47.5 МБ)']
 
-browser.implicitly_wait(20)
-# Скролим до кнопки, которая в центре страницы
-hidden_button = browser.find_element_by_xpath('//*[@id="container"]/div[2]/div/div[3]/div/div/div[2]/div/div[3]/a')
-browser.execute_script("return arguments[0].scrollIntoView(true);", hidden_button)
-hidden_button.click()
-
-# Список параметров для поиска эелементов
-links_text = ['Полная версия (294.40 MB)', 'Обновление (229.75 MB)', 'Полная версия (66.53 MB)',
-              'Скачать (6.4 МБ)', 'Скачать (47.5 МБ)']
-
-# Поиск элементов с последующей записью значений их атрибутов
-file = open(r'C:\Users\su.novikov\Desktop\links.txt', 'w', encoding='utf-8')
-for i in range(0, 5):
-    file.write(browser.find_element_by_link_text(links_text[i]).get_attribute('href') + '\n')
-file.close()
+    # Поиск элементов с последующей записью значений их атрибутов
+    file = open(r'C:\Users\su.novikov\Desktop\links.txt', 'w', encoding='utf-8')
+    for i in range(0, 5):
+        file.write(browser.find_element_by_link_text(links_text[i]).get_attribute('href') + '\n')
+        file.close()
+finally:
+    browser.close()
